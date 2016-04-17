85da0500f64691202987f148592baf266a3ee198
waren <waren@outlook.com>
2016-04-17 21:24:30 +0200

    first working version

--- spierdon/views.py
+++ spierdon/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-# Create your views here.
+
+@login_required
+def index(request):
+	return render(request, 'index.html')
+
