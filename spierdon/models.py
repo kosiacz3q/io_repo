98bd92bd94ab262f185181fc2a05ddd6c8339e3d
mateusz <fd>
2016-04-17 22:23:19 +0200

    dodanie levelu uzytkownika

--- spierdon/models.py
+++ spierdon/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class SpierdonUser(models.Model):
+    user = models.OneToOneField(User)
+    level = models.IntegerField(null=False, default=0)
+
+    def __unicode__(self):
+        return self.user.__str__() + ' ' + self.level.__str__() + ' lvl'
