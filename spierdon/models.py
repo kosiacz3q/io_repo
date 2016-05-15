f9d360ff3db675407a63a425c156d76e81500a4f
waren <waren@outlook.com>
2016-05-15 20:18:33 +0200

    Challenge class + exp field in User

--- spierdon/models.py
+++ spierdon/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 
 class SpierdonUser(models.Model):
     user = models.OneToOneField(User)
-    level = models.IntegerField(null=False, default=0)
+    # level = models.IntegerField(null=False, default=0)
+    exp = models.IntegerField(null=False, default=0)
+
+    def get_level(self):
+        return null
 
     def __str__(self):
         """
@@ -12,3 +16,12 @@ class SpierdonUser(models.Model):
         :return: string contains username and user level
         """
         return self.user.__str__() + ' ' + self.level.__str__() + ' lvl'
+
+
+class Challenge(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    exp = models.IntegerField(null=False, default=10)
+
+    def __str__(self):
+        return "%s (exp: %d)" % (self.name, self.exp)
