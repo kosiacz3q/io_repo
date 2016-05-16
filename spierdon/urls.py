01d2b1d98f054a719f59778777e2eb8b2b7ff1aa
waren <waren@outlook.com>
2016-05-16 17:29:40 +0200

    %s/rank/ranking/g + fixes

--- spierdon/urls.py
+++ spierdon/urls.py
@@ -7,5 +7,5 @@ URL patterns used by 'spierdon' app.
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<challenge_id>[0-9]+)/complete$', views.complete_challenge, name='complete_challenge'),
-    url(r'^rank/', views.rank, name='rank')
+    url(r'^ranking/', views.ranking, name='ranking')
 ]
