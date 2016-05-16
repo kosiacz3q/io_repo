a6d7a993c2ad49f4d7b11501b9dea98a7aea2a83
kosiacz3q <kosiak.lukasz@gmail.com>
2016-05-16 09:22:18 +0200

    NAvigatioon menu scaffold added

---
+++ spierdon/static/spierdon/main-page.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+    var menu_option_overview  = $("#menu-option-overview");
+    var menu_option_completed = $("#menu-option-completed");
+    var menu_option_challenges = $("#menu-option-challenges");
+
+    var overview_sector  = $(".overview-sector");
+    var completed_sector  = $(".completed-sector");
+    var challenges_sector  = $(".challenges-sector");
+
+    challenges_sector.hide(0);
+    $(".ranking-sector").hide(0);
+    completed_sector.hide(0);
+
+    menu_option_overview.click(function () {
+
+        if (overview_sector.is("Visible"))
+            return;
+
+        var active_section = challenges_sector;
+
+        if (completed_sector.is("Visible")){
+              active_section = completed_sector;
+        }
+        
+        active_section.fadeOut(100, function () {
+            overview_sector.fadeIn(100)
+        });
+    });
+
+    menu_option_challenges.click(function () {
+
+        if (challenges_sector.is("Visible"))
+            return;
+
+        var active_section = overview_sector;
+
+        if (completed_sector.is("Visible")){
+              active_section = completed_sector;
+        }
+
+        active_section.fadeOut(100, function () {
+            challenges_sector.fadeIn(100)
+        });
+    });
+
+    menu_option_completed.click(function () {
+
+        if (completed_sector.is("Visible"))
+            return;
+
+        var active_section = overview_sector;
+
+        if (challenges_sector.is("Visible")) {
+            active_section = challenges_sector;
+        }
+
+        active_section.fadeOut(100, function () {
+            completed_sector.fadeIn(100)
+        });
+    });
+});
