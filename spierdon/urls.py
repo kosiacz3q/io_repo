5cb162b2970616b137c1cf60f481d9e92711faef
Piotr Kurzawa <piotr.kurzawa@outlook.com>
2016-05-16 21:03:36 +0200

    url.py minor changes

--- spierdon/urls.py
+++ spierdon/urls.py
@@ -6,9 +6,9 @@ URL patterns used by 'spierdon' app.
 """
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<challenge_id>[0-9]+)/complete$', views.complete_challenge, name='complete_challenge'),
     url(r'^ranking/', views.ranking, name='ranking'),
+    url(r'^challenge/(?P<challenge_id>[0-9]+)/complete$', views.complete_challenge, name='complete_challenge'),
     url(r'^challenge/(?P<challenge_id>[0-9]+)/join/', views.join_challenge, name='join_challenge'),
     url(r'^challenge/new/', views.add_challenge, name='add_challange'),
-    url(r'^challenge/', views.get_challenges, name='get_challenges'),
+    url(r'^challenges/', views.get_challenges, name='get_challenges'),
 ]
